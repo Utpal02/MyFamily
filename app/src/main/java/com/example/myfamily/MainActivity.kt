17a5e8f7a60d09b088aabc23501ff54a7ef90573
Utpal Tiwari <utpaltiwari02@gmail.com>
2023-08-23 18:34:33 +0530

    Commit - 1

--- app/src/main/java/com/example/myfamily/MainActivity.kt
+++ app/src/main/java/com/example/myfamily/MainActivity.kt
@@ -14,16 +14,23 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
         val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
         bottomBar.setOnItemSelectedListener {menuItem->
-            if(menuItem.itemId == R.id.nav_guard)
-            {
-                inflateFragment(GuardFragment.newInstance())
-            }
-            else if(menuItem.itemId == R.id.nav_home)
-            {
-                inflateFragment(HomeFragment.newInstance())
+            when (menuItem.itemId) {
+                R.id.nav_guard -> {
+                    inflateFragment(GuardFragment.newInstance())
+                }
+                R.id.nav_home -> {
+                    inflateFragment(HomeFragment.newInstance())
+                }
+                R.id.nav_dashboard -> {
+                    inflateFragment(DashboardFragment.newInstance())
+                }
+                R.id.nav_profile -> {
+                    inflateFragment(ProfileFragment.newInstance())
+                }
             }
             true
         }
+        bottomBar.selectedItemId = R.id.nav_home
     }
 
 
